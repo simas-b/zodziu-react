09a11bb5f9ddd7843fa1d721ba24121956caee24
Simas Butavičius <simasbutavicius@gmail.com>
2022-02-10 12:20:07 +0200

    sentry

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/style.css";
 import App from "./App";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://3069b9b085434a358e8a980dc98d732a@o1140306.ingest.sentry.io/6197343",
+  integrations: [new BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
