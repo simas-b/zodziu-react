6fffa55ed3bb625aa98fc5d67b01ec48126022f0
Simas Butavičius <simasbutavicius@gmail.com>
2022-02-12 11:44:05 +0200

    padding

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -16,7 +16,7 @@ export default function Layout({ children }: Props) {
 
 const styles = {
   // main: ["h-full", "p-2", "items-center", "text-dark", "bg-white"],
-  main: ["h-full", "flex", "flex-col", "text-dark", "bg-white", "items-center", "justify-around"],
+  main: ["h-full", "flex", "flex-col", "text-dark", "bg-white", "items-center", "justify-around", "p-2"],
   container: [
     "flex",
     "flex-col",
