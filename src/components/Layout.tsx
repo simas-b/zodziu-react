33036bda4a174f886736438cfc0f6209ceec4c84
Simas Butavičius <simasbutavicius@gmail.com>
2022-02-12 11:39:18 +0200

    remove position fixed. it breaks iphone x

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,13 +8,15 @@ type Props = {
 export default function Layout({ children }: Props) {
   return (
     <main className={classNames(styles.main)}>
-      <div className={classNames(styles.container)}>{children}</div>
+      {/* <div className={classNames(styles.container)}>{children}</div> */}
+      {children}
     </main>
   );
 }
 
 const styles = {
-  main: ["h-full", "p-2", "items-center", "text-dark", "bg-white"],
+  // main: ["h-full", "p-2", "items-center", "text-dark", "bg-white"],
+  main: ["h-full", "flex", "flex-col", "text-dark", "bg-white", "items-center", "justify-around"],
   container: [
     "flex",
     "flex-col",
