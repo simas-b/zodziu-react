85d605b8a8b581ba18d5623ed146efacf2ce3054
PetrasNutautas <69975592+PetrasNutautas@users.noreply.github.com>
2022-03-30 16:20:12 +0300

    setHours not needed for currentDayStart

--- src/gameSetup.ts
+++ src/gameSetup.ts
@@ -3,7 +3,7 @@ import wordlist from "./wordlist";
 const getTodaysGameNumber = () => {
   const gameStart = new Date(2022, 1, 8, 0, 0, 0, 0);
   const currentDayStart = new Date().setHours(0, 0, 0, 0);
-  const distance = currentDayStart.getTime() - gameStart.getTime();
+  const distance = currentDayStart - gameStart.getTime();
 
   const daysPassed = Math.round(distance / (1000 * 60 * 60 * 24));
 
