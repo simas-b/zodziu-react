f3eaaea188bc0a914be28a8def57d0bc17de764f
Simas Butavičius <simasbutavicius@gmail.com>
2022-03-31 00:32:45 +0300

    ukraine colors

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <span className="border-yellow border-b-2">
       <span className="text-yellow">ŽÓ</span>
-      <span className="text-dark">DŽIU</span>
+      <span className="text-blue-400">DŽIU</span>
     </span>
   );
 }
